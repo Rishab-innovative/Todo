7d619bebe892c63ea9b918343a97002859df9c9c
alkainno <alka.chauhan@innotechteam@gmail.com>
2023-09-19 19:19:41 +0530

    task4 done

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,49 +1,47 @@
 import React, { useState } from "react";
-import Badge from "react-bootstrap/Badge";
-import Form from "react-bootstrap/Form";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
 import "./App.css";
+import TodoItem from "./TodoItem";
 
 const Todo: React.FC = () => {
   const [data, setData] = useState<string[]>(["shopping", "homeWork"]);
   const [checks, setChecks] = useState<boolean[]>([false, false]);
   const [inputValue, setInputValue] = useState<string>("");
 
-  const _handleCheck = (index: number) => {
+  const handleCheck = (index: number) => {
     const newChecks: boolean[] = [...checks];
     newChecks[index] = !newChecks[index];
     setChecks(newChecks);
   };
 
-  const _add = () => {
+  const addTodo = () => {
     if (inputValue === "") {
       const temp: any = document.querySelector<HTMLElement>(".inputField");
       temp.style.borderColor = "red";
     } else {
-      data.unshift(inputValue);
-      setData([...data]);
+      const newData = [inputValue, ...data];
+      const newChecks = [false, ...checks];
+      setData(newData);
+      setChecks(newChecks);
       setInputValue("");
+      const temp: any = document.querySelector<HTMLElement>(".inputField");
+      temp.style.borderColor = "black";
     }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+    const temp: any = document.querySelector<HTMLElement>(".inputField");
+    temp.style.borderColor = "black";
   };
 
   return (
     <div className="App">
       <div className="container">
-        <h3>Todo App</h3>
-
-        {data.map((item, index) => (
-          <InputGroup key={index} className="mb-3">
-            <InputGroup.Checkbox
-              onChange={() => _handleCheck(index)}
-              checked={checks[index]}
-            />
-            <Form.Control value={item} readOnly />
-            {checks[index] ? <Badge bg="secondary">complete</Badge> : ""}
-          </InputGroup>
-        ))}
+        <TodoItem data={data} checks={checks} handleCheck={handleCheck} />
 
         <h3>Todo</h3>
         <InputGroup className="mb-3">
@@ -53,14 +51,15 @@ const Todo: React.FC = () => {
             value={inputValue}
             aria-label="Default"
             aria-describedby="inputGroup-sizing-default"
-            onChange={(event) => setInputValue(event.target.value)}
+            onChange={handleInputChange}
           />
         </InputGroup>
-        <Button variant="light" className="custom-button" onClick={_add}>
+        <Button variant="light" className="custom-button" onClick={addTodo}>
           Submit
         </Button>
       </div>
     </div>
   );
 };
+
 export default Todo;
